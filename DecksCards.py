75eb47f742f0701bc0cac6a129b624061aaa3f14
Frederik Franzen <frfranzen@gmail.com>
2024-09-23 13:33:14 +0200

    learn

---
+++ DecksCards.py
@@ -0,0 +1,13 @@
+from classes import *
+
+Deck1 = Deck("Deck1")
+
+card1 = Card("front1", "back1")
+card2 = Card("front2", "back2")
+card3 = Card("front3", "back3")
+
+Deck1.addCard(card1)
+Deck1.addCard(card2)
+Deck1.addCard(card3)
+
+Deck1.learnDeck()
--- classes.py
+++ classes.py
@@ -15,6 +15,21 @@ class Card:
     def __str__(self):
         return "ID: " + str(self.id) + " DATE: " + str(self.date) + " FRONT: " + self.front + " BACK: " + self.back
 
+    def showCard(self):
+        print(self.front)
+        input("Press Enter to show back...")
+        print(self.back)
+        answer = input("Did you know the card? (yes/no)" )
+        if answer == "yes":
+            self.date = (datetime.datetime.now() + datetime.timedelta(days=5)).date()
+            print("Nice Work! you will see this card again: " + str(self.date))
+        elif answer == "no":
+            self.date = (datetime.datetime.now() + datetime.timedelta(days=1)).date()
+            print("Better luck next time! you will see this card again: " + str(self.date))
+        else:
+            print("ERROR")
+
+
 GlobalIDDeck = 0
 class Deck:
     def __init__(self, Name):
@@ -30,3 +45,9 @@ class Deck:
 
     def __str__(self):
         return "ID: " + str(self.id) + " NAME: " + self.Name + " CARDS: " + str(self.cards)
+
+    def learnDeck(self):
+        date = datetime.datetime.now().date()
+        for card in self.cards:
+            if card.date == date:
+                card.showCard()
